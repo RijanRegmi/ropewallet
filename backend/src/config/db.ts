6b77ebfb9d91ba2805cbfed7b9c34e6d59c2ebc2
Rijan Regmi <rijanregmi111@gmail.com>
2026-08-02 17:41:03 +0545

    solve issue of backend

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,7 +1,10 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import path from 'path';
 
 dotenv.config();
+dotenv.config({ path: path.resolve(process.cwd(), '../.env') });
+dotenv.config({ path: path.resolve(process.cwd(), '.env') });
 
 let isConnected = false;
 
